68734a6c8c7be9591b238ef374e8e3ebbcf2c088
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-22 14:40:36 -0600

    refactored to add the namespacing convention

--- history/views.py
+++ history/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render
-from .models import Artist
-from .models import Song
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from .models import Artist, Song
 
 # Create your views here.
 
 def index(request):
     """The home page for music history"""
-    artist_list = Artist.objects.order_by('-name')
+    artist_list = Artist.objects.order_by('name')
     # song_list = Song.objects.order_by('-name')
     context = {
         'artist_list': artist_list,
-        # 'song_list': song_list,
         }
     return render(request, 'history/index.html', context)
 
-def detail (request, artist_id):
-    try:
-        artist = Artist.objects.get(pk=artist_id)
-    except Artist.DoesNotExist:
-        raise Http404("Artist does not exist")
+# def detail (request, artist_id):
+#     try:
+#         artist = Artist.objects.get(pk=artist_id)
+#     except Artist.DoesNotExist:
+#         raise Http404("Artist does not exist")
+#     return render(request, 'history/detail.html', {'artist': artist})
+
+def detail(request, artist_id):
+    artist = get_object_or_404(Artist, pk=artist_id)
     return render(request, 'history/detail.html', {'artist': artist})
