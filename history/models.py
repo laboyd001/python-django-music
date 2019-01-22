bdedd7ed72c0129e026994b15aa4a27af82ae33b
Lesley Boyd <lesley.a.boyd1@gmail.com>
2019-01-22 14:51:20 -0600

    added in a doc string for the details method  and removed commented out code

--- history/models.py
+++ history/models.py
@@ -2,7 +2,6 @@ from django.db import models
 
 # Create your models here.
 
-
 class Artist(models.Model):
     """Name of a musical artist"""
     name = models.CharField(max_length=200)
--- history/views.py
+++ history/views.py
@@ -8,19 +8,12 @@ from .models import Artist, Song
 def index(request):
     """The home page for music history"""
     artist_list = Artist.objects.order_by('name')
-    # song_list = Song.objects.order_by('-name')
     context = {
         'artist_list': artist_list,
         }
     return render(request, 'history/index.html', context)
 
-# def detail (request, artist_id):
-#     try:
-#         artist = Artist.objects.get(pk=artist_id)
-#     except Artist.DoesNotExist:
-#         raise Http404("Artist does not exist")
-#     return render(request, 'history/detail.html', {'artist': artist})
-
 def detail(request, artist_id):
+    """the artist detail page"""
     artist = get_object_or_404(Artist, pk=artist_id)
     return render(request, 'history/detail.html', {'artist': artist})
